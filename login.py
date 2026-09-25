2002b681852ea5ea4fec9fb9e0a0cda9fc896bb9
Karsten Valentin <kenvalentin69@gmail.com>
2026-09-25 22:52:11 +0800

    test: update login condition

--- login.py
+++ login.py
@@ -3,7 +3,7 @@ def authenticate_user(username, password):
     if not username or not password:
         return False
 
-    if username == "student" and password == "password":
+    if username == "teacher" and password == "teacher123":
         return True
 
     return False
